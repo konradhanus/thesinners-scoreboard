aa8537746daa8c9c41854c8a3eaf186ae822795b
Filip <filiptankiewicz04@gmail.com>
2022-07-26 11:42:03 +0200

    first component & redux installed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import ScoreBoard from "./components/ScoreBoard";
 
-test('renders learn react link', () => {
-  render(<App />);
+test("renders learn react link", () => {
+  render(<ScoreBoard />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/components/ScoreBoard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.css';
+
+function ScoreBoard() {
+  return (
+    <div style={{ textAlign: "center"}}>
+      <h1>Siema</h1>
+    </div>
+  );
+}
+
+export default ScoreBoard;
