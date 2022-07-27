6a4621737ee37d59fb7afbdb1881e3830c9db056
Filip <filiptankiewicz04@gmail.com>
2022-07-27 16:07:55 +0200

    firebase save mechanism

--- src/components/ScoreBoard/index.js
+++ src/components/ScoreBoard/index.js
@@ -6,8 +6,9 @@ import { scoreData } from "./scoreData";
 import ScoreTable from "./Table";
 import Button from "./Button";
 import IncrementButton from "../IncrementButton";
-import { getDatabase, ref, set } from "firebase/database";
-import { app as firebaseApp } from '../../firebase';
+import { getDatabase, set, ref, get } from "firebase/database";
+import uuid from "react-uuid";
+import { app as firebaseApp } from "../../firebase";
 import {
   TableContainer,
   Wrapper,
@@ -15,7 +16,6 @@ import {
   MessageContainer,
   StatusContainer,
 } from "./style";
-
 
 function ScoreBoard(props) {
   const [count, setCount] = useState(0);
@@ -27,38 +27,46 @@ function ScoreBoard(props) {
   console.log("Status: ", props.change);
   console.log("Counter: ", count);
 
-// Create a reference to the file we want to download
-// 
-const database = getDatabase(firebaseApp);
+  const database = getDatabase(firebaseApp);
 
-function setData() {
+  function setData() {
+    const uuidKey = uuid();
 
- set(ref(database, 'public/'), {
-  players: {
-    name: "Alicja",
-    age: 22,
-    description: "Hi, i like this",
-    points: props.change
-  },
- }).then(() => {
-  alert("Twoje dane zostały zapisane")
- })
- .catch((error) => {
-  alert(error + "Nie zapisało się")
- });
+    set(ref(database, "public/" + "players/" + "/" + uuidKey), {
+      player: {
+        name: "Alicja",
+        age: 22,
+        description: "Hi, i like this",
+        points: props.change,
+      },
+    })
+      .then(() => {
+        alert("Twoje dane zostały zapisane");
+      })
+      .catch((error) => {
+        alert(error + "Nie zapisało się");
+      });
+  }
 
-}
+  // function getDatabase() {
+  //   const data = "https://rankingdog-default-rtdb.firebaseio.com/public/data.json?print=pretty"
+  // }
 
+  function getData() {
+    get(ref(database, "public")).then((snapshot) => {
+      const players = [];
 
-// Get the download URL
-// getDownloadURL(starsRef)
-//   .then((url) => {
-//     // Insert url into an <img> tag to "download"
-//   })
-//   .catch((error) => {
-//     alert(error)
-//   });
-  
+      snapshot.forEach((childSnapshot) => {
+        players.push(childSnapshot.val());
+      });
+
+      alert("Gracze");
+
+      console.log("gracze:", players, snapshot);
+
+      return <div>{players}</div>;
+    });
+  }
   return (
     <TableContainer>
       <h1>ScoreBoard</h1>
@@ -69,8 +77,11 @@ function setData() {
             value="Zwiększ o 1"
             onClick={() => setCount(count + 1)}
           />
-          <h3 style={{ marginTop: 60, padding: 40, textAlign: "center" }} className="title is-3">
-            Twoja liczba: 
+          <h3
+            style={{ marginTop: 60, padding: 40, textAlign: "center" }}
+            className="title is-3"
+          >
+            Twoja liczba:
             <br></br>
             {count}
           </h3>
@@ -78,10 +89,8 @@ function setData() {
             value="Zmniejsz o 1"
             onClick={() => setCount(count - 1)}
           />
-          <IncrementButton
-            value="Zapisz"
-            onClick={() => setData()}
-          />
+          <IncrementButton value="Zapisz" onClick={() => setData()} />
+          <IncrementButton value="Odczytaj" onClick={() => getData()} />
         </Increment>
         <MessageContainer>
           {scoreData.map((item, key) => {
